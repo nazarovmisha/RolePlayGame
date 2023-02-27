6cd49e3a4e67c8148ebd1de9e8bef64db010974e
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-02-27 13:26:33 +0300

    RoleGame

--- src/Battle.java
+++ src/Battle.java
@@ -13,8 +13,13 @@ public class Battle {
         }
     }
 
-    public boolean makeHit(Entity attacker, Entity defender, Entity){
+    public boolean makeHit(Entity attacker, Entity defender){
 int hit = attacker.fight();
 int defenderHealth = defender.getHealth()-hit;
+if (hit !=0){
+    System.out.println(String.format("%s атаковал, удар %d единиц здоровья!", attacker.getName(), hit));
+    System.out.println(String.format("У %d осталось %s единиц здоровья!", defender.getName(), defenderHealth));
+}
+        return false;
     }
 }
