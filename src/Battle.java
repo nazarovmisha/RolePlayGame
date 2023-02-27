47ab555ea902de6edd4c5a4ffdda3c28e4a6c0b2
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-02-27 13:38:07 +0300

    RoleGame

--- src/Battle.java
+++ src/Battle.java
@@ -1,25 +1,33 @@
 public class Battle {
-    public void fight(Entity monster,Entity hero){
-        int turn=1;
-        boolean isFightEnded=false;
-        while (!isFightEnded){
+    public void fight(Entity monster, Entity hero) {
+        int turn = 1;
+        boolean isFightEnded = false;
+        while (!isFightEnded) {
             System.out.println("Ход" + turn + "!!!");
 
-            if (turn++%2!=0){
-                isFightEnded=makeHit(monster,hero);
-            }else {
-                isFightEnded=makeHit(hero,monster);
+            if (turn++ % 2 != 0) {
+                isFightEnded = makeHit(monster, hero);
+            } else {
+                isFightEnded = makeHit(hero, monster);
             }
         }
     }
 
-    public boolean makeHit(Entity attacker, Entity defender){
-int hit = attacker.fight();
-int defenderHealth = defender.getHealth()-hit;
-if (hit !=0){
-    System.out.printf("%s атаковал, удар %d единиц здоровья!%n", attacker.getName(), hit);
-    System.out.printf("У %s осталось %d единиц здоровья!%n", defender.getName(), defenderHealth);
-}
+    public boolean makeHit(Entity attacker, Entity defender) {
+        int hit = attacker.fight();
+        int defenderHealth = defender.getHealth() - hit;
+        if (hit != 0) {
+            System.out.printf("%s атаковал, удар %d единиц здоровья!%n", attacker.getName(), hit);
+            System.out.printf("У %s осталось %d единиц здоровья!%n", defender.getName(), defenderHealth);
+        } else {
+            System.out.printf("%s промахнулся, урона нет!", attacker.getName());
+        }if (defenderHealth<=0&&defender instanceof Hero){
+            System.out.println("Вы пали в бою!Враг победил!!!");
+        }else if(defenderHealth<=0){
+            System.out.println("Враг повержен! Пир на весь мир!!!");
+            System.out.printf("Вы получаете %d единиц опыта и %d единиц золота%n", defender.getExperience(), defender.getGold());
+
+        }
         return false;
     }
 }
